361c6c7ad2a36e69df22d4e0dcda0bfd76906c5e
Adam Liter <io@adamliter.org>
2025-01-31 00:59:40 -0500

    feat: add /healthz endpoint to server

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,14 @@
 // -*- mode: rustic; coding: utf-8; fill-column: 88; -*-
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-
-    format!("Hello {}!", &name)
+async fn healthz() -> impl Responder {
+    HttpResponse::Ok()
 }
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-    })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().route("/healthz", web::get().to(healthz)))
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
