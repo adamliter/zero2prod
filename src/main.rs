4f260a3a38035a72e2c3466bf4ab2e9111f77b60
Adam Liter <io@adamliter.org>
2025-02-17 16:53:46 -0700

    feat(logging): configure basic logging

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // -*- mode: rustic; coding: utf-8; fill-column: 88; -*-
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -7,6 +8,8 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 // -*- mode: rustic; coding: utf-8; fill-column: 88; -*-
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware::Logger, web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -11,6 +11,7 @@ pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Er
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/healthz", web::get().to(healthz))
             .route("/subscriptions", web::post().to(subscribe))
             .app_data(db_pool.clone())
