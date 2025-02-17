fc4920f2d000cc2c672353a3c20f96b70de8f715
Adam Liter <io@adamliter.org>
2025-02-17 11:54:17 -0700

    feat: add /subscriptions to parse URL encoded data

--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,27 @@ use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 use std::net::TcpListener;
 
 async fn healthz() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server =
-        HttpServer::new(|| App::new().route("/healthz", web::get().to(healthz)))
-            .listen(listener)?
-            .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/healthz", web::get().to(healthz))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
 
     Ok(server)
 }
