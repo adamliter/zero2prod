dd6c78e091b34c5d06ede8336054efedeeb17984
Adam Liter <io@adamliter.org>
2025-02-17 12:12:53 -0700

    refactor: separate into modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,16 @@
 // -*- mode: rustic; coding: utf-8; fill-column: 88; -*-
+pub mod configuration;
+pub mod routes;
+
 use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpServer};
 use std::net::TcpListener;
-
-async fn healthz() -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/healthz", web::get().to(healthz))
-            .route("/subscriptions", web::post().to(subscribe))
+            .route("/healthz", web::get().to(routes::healthz))
+            .route("/subscriptions", web::post().to(routes::subscribe))
     })
     .listen(listener)?
     .run();
