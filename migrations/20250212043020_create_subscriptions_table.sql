0c52e924df0024246f362ee339159d799677bfa4
Adam Liter <io@adamliter.org>
2025-02-17 12:09:33 -0700

    feat: add sqlx migrations and Docker DB

---
+++ migrations/20250212043020_create_subscriptions_table.sql
@@ -0,0 +1,8 @@
+-- -*- mode: sql; coding: utf-8; fill-column: 80; -*-
+CREATE TABLE subscriptions (
+    id uuid NOT NULL
+    , PRIMARY KEY (id)
+    , email TEXT NOT NULL UNIQUE
+    , name TEXT NOT NULL
+    , subscribed_at timestamptz NOT NULL
+);
