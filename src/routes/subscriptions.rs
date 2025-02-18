c6c16966448046e1c5b6be5421d77441f9dd09e1
Adam Liter <io@adamliter.org>
2025-02-17 17:43:38 -0700

    feat(logging): add logging to /subscriptions

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -14,6 +14,13 @@ pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
 ) -> impl Responder {
+    let request_id = Uuid::new_v4();
+    log::info!(
+        "request_id {} - Adding {} <{}> as new subscriber.",
+        request_id,
+        form.name,
+        form.email,
+    );
     match sqlx::query!(
         r#"
 INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -26,9 +33,20 @@ VALUES ($1, $2, $3, $4)"#,
     .execute(pool.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id,
+            );
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            eprintln!("Failed to execute query: {}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
+
             HttpResponse::InternalServerError().finish()
         }
     }
