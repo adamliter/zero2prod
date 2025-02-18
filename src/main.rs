2939bd6344c52c70e95a58523a60836fd8bd9f35
Adam Liter <io@adamliter.org>
2025-02-17 18:57:54 -0700

    feat(tracing): ensure other loggers are picked up
    The tracing-log package allows us to register a logger implementation
    that sends logs from other packages to the tracing subscriber for
    processing.

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use sqlx::PgPool;
 use std::net::TcpListener;
 use tracing::subscriber::set_global_default;
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use zero2prod::configuration::get_configuration;
@@ -10,6 +11,8 @@ use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    LogTracer::init().expect("Failed to set logger.");
+
     let env_filter =
         EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new("info"));
 
